8ba5aa92e988f4dcae2851a74759b5b2cdc6186b
Aneeza Junaid <124883425+aneezajunaid@users.noreply.github.com>
2024-11-22 16:30:13 +0500

    Update Project.java

--- SCD_Backend/src/Project.java
+++ SCD_Backend/src/Project.java
@@ -15,13 +15,46 @@ public class Project {
     private Scdfinal.Model model;
     private CodeGenerator codeGenerator;
 
-    public void save() {
-        // Implementation to be added
-    }
+  public void exportToPNG(String filePath) throws IOException {
+            BufferedImage image = exportToBufferedImage();
+            ImageIO.write(image, "png", new File(filePath));
+        }
+        /**
+         * Exports the use case diagram as a JPEG image
+         * @param filePath Path where the JPEG will be saved
+         * @throws IOException If there's an error writing the file
+         */
+        public void exportToJPEG(String filePath) throws IOException {
+            BufferedImage image = exportToBufferedImage();
+            ImageIO.write(image, "jpg", new File(filePath));
+        }
 
-    public void load() {
-        // Implementation to be added
-    }
+        /**
+         * Creates a BufferedImage of the entire diagram
+         * @return BufferedImage of the diagram
+         */
+        private BufferedImage exportToBufferedImage() {
+            // Create a buffered image of the diagram's exact size
+            BufferedImage image = new BufferedImage(getWidth(), getHeight(),
+                    BufferedImage.TYPE_INT_RGB);
+
+            // Create graphics context
+            Graphics2D g2d = image.createGraphics();
+
+            // Optional: Set rendering hints for better quality
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                    RenderingHints.VALUE_RENDER_QUALITY);
+
+            // Paint the diagram onto the buffered image
+            paint(g2d);
+
+            // Dispose of graphics context
+            g2d.dispose();
+
+            return image;
+        }
 }
 
 
