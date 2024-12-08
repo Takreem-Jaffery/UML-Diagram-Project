e0f765882d9be57a3b6a80b8afaf1db3fa357287
Miraal Fatima <anonymous.bean07@gmail.com>
2024-12-08 20:40:43 +0500

    added final java docs files

---
+++ SCD_Project/src/Test/ClassDiagramTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import business.ClassDiagram;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ClassDiagramTest {
+
+    @Test
+    void testDisplay() {
+        ClassDiagram classDiagram = new ClassDiagram();
+        Assertions.assertDoesNotThrow(classDiagram::display, "The display method should not throw an exception.");
+    }
+}
---
+++ SCD_Project/src/Test/UsecaseTest.java
@@ -0,0 +1,38 @@
+package Test;
+
+import business.Usecase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UsecaseTest {
+
+    @Test
+    void testUsecase() {
+
+        String initialName = "TestUsecase";
+        Point initialPosition = new Point(50, 100);
+        Usecase usecase = new Usecase(initialName, initialPosition);
+
+        String name = usecase.getName();
+        Point position = usecase.getPosition();
+        String diagramNotes = usecase.getDiagramNotes();
+
+        assertEquals(initialName, name, "The name should be the same as the initial name.");
+        assertEquals(initialPosition, position, "The position should be the same as the initial position.");
+        assertEquals(initialName + "\n", diagramNotes, "The diagram notes should contain the name of the usecase.");
+
+        String newName = "UpdatedUsecase";
+        Point newPosition = new Point(150, 200);
+        usecase.setName(newName);
+        usecase.setPosition(newPosition);
+
+        Assertions.assertEquals(newName, usecase.getName(), "The name should be updated.");
+        Assertions.assertEquals(newPosition, usecase.getPosition(), "The position should be updated.");
+
+        Assertions.assertEquals(newName + "\n", usecase.getDiagramNotes(), "The diagram notes should reflect the updated name.");
+    }
+}
